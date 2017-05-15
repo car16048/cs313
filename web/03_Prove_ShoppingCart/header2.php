1af37d6fab562a714ec2408aad7393d67935202c
Benjamin C <car16048@byui.edu>
2017-05-15 07:33:21 -0700

    Allow scripts loaded over HTTPS

--- web/03_Prove_ShoppingCart/header2.php
+++ web/03_Prove_ShoppingCart/header2.php
@@ -1,7 +1,7 @@
 </title>
-		<link rel="StyleSheet" href="http://code.jquery.com/ui/1.12.1/themes/start/jquery-ui.css" type="text/css" />
+		<link rel="StyleSheet" href="//code.jquery.com/ui/1.12.1/themes/start/jquery-ui.css" type="text/css" />
 		<link rel="StyleSheet" href="../styles/site.css" type="text/css" />
-		<script type="text/javascript" src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
-		<script type="text/javascript" src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+		<script type="text/javascript" src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script type="text/javascript" src="//code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 	</head>
 	<body>
